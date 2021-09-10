b7bbfc7d9f0d962f1afb61a8f21eaf663d8d71ea
Lokeswari Satyanarayana <lokeswari.satyanarayana@mutualmobile.com>
2021-09-10 20:11:57 +0530

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Lokeswari Satyanarayana on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
